7f83f1af9f51adce35cef800f9d34f71e3afdd2b
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-09-27 18:18:19 -0600

    Nav bar structure

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,28 @@
 import React from "react";
-// import "./Navigation.scss"
+import "./Navigation.scss"
 
 export default function Navigation(props) {
   return (
     <nav>
-      I am a navigation component
+      <div className="nav--project-name">GyManager</div>
+
+      <div className="nav-bar">
+        <ul className="nav-bar-left">
+          <li>Gym Capacity</li>
+          <li>Book</li>
+        </ul>
+        <div className="nav-bar-right">
+          <div className="login--register">
+            <button>Login</button>
+            <button>Register</button>
+          </div>
+          <div className="login-logout">
+            <label>Admin</label>
+            <button>LogOut</button>
+          </div>
+        </div>
+
+      </div>
     </nav>
   );
 }
