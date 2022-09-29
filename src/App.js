5e6673fdf80415baf94bc8449e242f76e8ed95bb
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-09-28 20:13:35 -0600

    install react router

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import BulletinBoard from './components/BulletinBoard';
 import GymCapacity from './components/GymCapacity';
 import Navigation from './components/Navigation';
 import React, { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
 
   const [currentNavSelection, setCurrentNavSelection] = useState("gym capacity");
 
   return (
-    <div className="App">
-      <Navigation onChange={val => setCurrentNavSelection(val)}/>
-      <BulletinBoard />
-      { currentNavSelection === "gym capacity" && <GymCapacity /> }
-      { currentNavSelection === "book" && <Booking /> }
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navigation onChange={val => setCurrentNavSelection(val)} />
+        <BulletinBoard />
+        {currentNavSelection === "gym capacity" && <GymCapacity />}
+        {currentNavSelection === "book" && <Booking />}
+      </div>
+    </BrowserRouter>
   );
 }
 
