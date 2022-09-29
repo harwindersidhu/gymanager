e6a85fe435521922f70bc211c0a855c16599e45d
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-09-29 16:58:15 -0600

    Add doughnut graph to show present hour capacity with fake data

--- src/components/component_capacity/GraphPresentHour.js
+++ src/components/component_capacity/GraphPresentHour.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Doughnut } from "react-chartjs-2";import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: ['Filled', 'Remaining'],
+  datasets: [
+    {
+      data: [38, 12],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)'
+      ],
+      borderWidth: 1
+    }
+  ]
+};
 
 export default function GraphPresentHour(props) {
   return (
     <div className="graph-present-hour">
-      I am a present hour graph
+      {/* <Doughnut data={data} /> */}
+      <Doughnut data={data}/>
     </div>
   );
 }
