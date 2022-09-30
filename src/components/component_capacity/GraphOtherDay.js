1eeec490caf54aef46dcd04efd2e1c1ac4df0c5c
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-09-29 21:59:44 -0600

    Add date picker

--- src/components/component_capacity/GraphOtherDay.js
+++ src/components/component_capacity/GraphOtherDay.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function GraphOtherDay(props) {
+
+  const [ date, setDate ] = useState("");
+  // useEffect(() => {
+  //   document.title = date
+  // }, [date]);
+
   return (
     <div className="graph-other-day">
-      I am a other day graph
+      <p className="selected_date_label">Graph for date: {date}</p>
+      <label>Choose a date for which you want to see the capacity:</label>
+      <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+      
     </div>
   );
 }
