59567179f0caac0452460fe85fefb403645dca25
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-10-03 13:09:27 -0600

    Show two graphs at same time

--- src/components/component_capacity/Graphs.js
+++ src/components/component_capacity/Graphs.js
@@ -1,23 +1,38 @@
 import React, { useState } from "react";
-import GraphOtherDay from "./GraphOtherDay";
+// import GraphOtherDay from "./GraphOtherDay";
 import GraphPresentDay from "./GraphpresentDay";
 import GraphPresentHour from "./GraphPresentHour";
 import "./Graphs.scss";
-import SelectType from "./SelectType";
+// import SelectType from "./SelectType";
+
+function todayDate() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  today = yyyy + '-' + mm + '-' + dd;
+  return today;
+}
 
 export default function Graphs(props) {
 
-  const [graphType, setGraphType] = useState("presentHour");
+  // const [graphType, setGraphType] = useState("presentHour");
+  const today = todayDate();
+  const [date, setDate] = useState(today);
 
   return (
     <div className="graphs-view">
-      <div className="select-view">
-        <SelectType value={graphType} onChange={(val) => setGraphType(val)} />
+      <div className="graph-one">
+        {/* <SelectType value={graphType} onChange={(val) => setGraphType(val)} /> */}
+        <GraphPresentHour />
       </div>
-      <div className="graph">
-        {graphType === "presentHour" && <GraphPresentHour />}
-        {graphType === "presentDay" && <GraphPresentDay />}
-        {graphType === "otherDay" && <GraphOtherDay />}
+      <div className="graph-two">
+        {/* {graphType === "presentHour" && <GraphPresentHour />}
+        {graphType === "presentDay" && <GraphPresentDay />} */}
+        {/* {graphType === "otherDay" && <GraphOtherDay />} */}
+        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+        <GraphPresentDay />
       </div>
     </div>
   );
