5697cdf34b8ffe7ccaaadf64a7cb4cb3994bcf89
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-10-07 01:23:59 -0600

    Add Select to choose value for time, Add labels to graphs

--- src/components/component_capacity/Graphs.js
+++ src/components/component_capacity/Graphs.js
@@ -8,12 +8,13 @@ export default function Graphs(props) {
   return (
     <div className="graphs-view">
       <div className="graph-one">
-        {/* <SelectType value={graphType} onChange={(val) => setGraphType(val)} /> */}
         <GraphPresentHour filled={props.presentHourData} remaining={50 - props.presentHourData}/>
+        <label className="label-graph-one">Capacity Present Hour</label>
       </div>
       <div className="graph-two">
         <input type="date" value={props.date} onChange={(e) => props.setDate(e.target.value)} />
         <GraphPresentDay labels={Object.keys(props.presentDayData)} data={Object.values(props.presentDayData)}/>
+        <label className="label-graph-two">Select date to see capacity of that date</label>
       </div>
     </div>
   );
