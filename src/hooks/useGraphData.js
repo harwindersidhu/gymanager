24afe5d5b80608bef3ef602f30d5dd5baca1568a
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-10-12 14:59:28 -0600

    Fixed css and graph error

--- src/hooks/useGraphData.js
+++ src/hooks/useGraphData.js
@@ -18,7 +18,7 @@ export default function useGraphData() {
 
   const [presentHourData, setPresentHourData] = useState(0);
 
-  const initialDayData = {"7am": 0, "8am": 0, "9am": 0, "10am": 0, "11am": 0, "12pm": 0, "1pm": 0, "2pm": 0, "3pm": 0, "4pm": 0, "5pm": 0, "6pm": 0, "7pm": 0, "8pm": 0, "9pm": 0}
+  const initialDayData = { "7am": 0, "8am": 0, "9am": 0, "10am": 0, "11am": 0, "12pm": 0, "1pm": 0, "2pm": 0, "3pm": 0, "4pm": 0, "5pm": 0, "6pm": 0, "7pm": 0, "8pm": 0, "9pm": 0 }
   const [presentDayData, setPresentDayData] = useState(initialDayData);
 
   useEffect(() => {
@@ -34,21 +34,20 @@ export default function useGraphData() {
       const gymCapacityArray = all[1].data.gymCapacity;
       for (const obj of gymCapacityArray) {
         const key = obj.time;
-        const value = obj.number_of_people;
-        // setPresentDayData({
-        //   ...presentDayData,
-        //   key: value 
-        // })
-        setPresentDayData( prev => {
-          let dataObj = { ...prev };
-          dataObj[key] = value;
-          return dataObj;
-        })
+        if (key !== "") {
+          const value = obj.number_of_people;
+          setPresentDayData(prev => {
+            let dataObj = { ...prev };
+            dataObj[key] = value;
+            return dataObj;
+          })
+        }
+
       }
     })
-    .catch((e) => {
-      console.log("Promise error: ", e)
-    })
+      .catch((e) => {
+        console.log("Promise error: ", e)
+      })
   }, [date, presentHourData]);
 
   function saveCapacity(time, numberOfPeople) {
@@ -64,7 +63,7 @@ export default function useGraphData() {
       time: time,
       number_of_people: numberOfPeople
     };
-    
+
     return axios.post(`/api/capacity`, capacityData)
       .then((response) => {
         console.log("Response from saving capacity: ", response.data.gymCapacity);
