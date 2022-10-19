2aa9b05a3a4a30b94de9e9f2ff31bed6ccf2693d
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-10-18 21:53:21 -0600

    Remove extra lines

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import './App.css';
 import Booking from './components/component_booking/Booking';
 import GymCapacity from './components/component_capacity/GymCapacity';
 import Navigation from './components/Navigation';
-import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BulletinBoardProvider from './providers/BulletinBoardProvider';
 import Login from './components/Login';
-import { loginContext } from './providers/LoginProvider';
 import ContactUs from './components/component_contact/ContactUs';
 import Register from './components/Register';
 
@@ -16,8 +14,6 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navigation />
-        {/* {user.email === "" && <Login />}
-        {user.email !== "" && <BulletinBoardProvider> */}
         <BulletinBoardProvider>
           <Routes>
             <Route path='/register' element={<Register />} />
--- src/components/component_capacity/GraphPresentHour.js
+++ src/components/component_capacity/GraphPresentHour.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Doughnut } from "react-chartjs-2";import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function GraphPresentHour(props) {
